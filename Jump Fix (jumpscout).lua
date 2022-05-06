df172413ae3bcefd78b40b90d4dcb5ef427d60b6
bocho <54505417+Bochocho3001@users.noreply.github.com>
2022-05-06 16:34:50 -0300

    Update Jump Fix (jumpscout).lua

--- Jump Fix (jumpscout).lua
+++ Jump Fix (jumpscout).lua
@@ -1,10 +1,12 @@
 local bit = require("bit")
 
-local bor, band = bit.bor, bit.band
-
 local weapons = Menu.MultiCombo("Jump Fix", "Active Weapons", {"Knife", "Nades", "Scout", "Awp", "Autsniper", "Pistol", "Heavy Pistol", "Global"}, 0, "Deactivates Auto Strafe When Jumping With The Selected Weapons")
 local hc_on = Menu.Switch("Jump Fix", "Override Hitchance", false)
 local hc = Menu.SliderInt("Jump Fix", "Hitchance On Jump", 0, 0, 100)
+
+hc_on:RegisterCallback(function()
+    hc:SetVisible()
+end)
 
 local auto_strafe = Menu.FindVar("Miscellaneous", "Main", "Movement", "Auto Strafe")
 
@@ -20,74 +22,44 @@ function jump_fix()
     local weap = me:GetActiveWeapon()
 	if weap == nil then return end
     local weap_id = weap:GetClassId()
-    local is_nade = weap:IsGrenade()
-    local is_knife = weap:IsKnife()
-    local is_pistol = weap:IsPistol()
-    local is_gun = weap:IsGun()
-    local is_ssg = weap_id == 267
-    local is_awp = weap_id == 233
-    local is_auto = weap_id == 242 or weap_id == 261
-    local is_heavy_pistol = weap_id == 46
+    local weapon_list = {
+        is_knife = weap:IsKnife(),
+        is_nade = weap:IsGrenade(),
+        is_ssg = weap_id == 267,
+        is_awp = weap_id == 233,
+        is_auto = weap_id == 242 or weap_id == 261,
+        is_pistol = weap:IsPistol(),
+        is_heavy_pistol = weap_id == 46,
+        is_gun = weap:IsGun(),
+    }
 
     local player = EntityList.GetLocalPlayer()
     if player == nil then return end
 
-    local get_multiCombo = function(number)
-        local shift = bit.lshift(1, number)
-        return bit.band(weapons:Get(), shift) > 0 and true or false
+    if velocity(player) < 5 then
+        for i in weapon_list do
+            if weapons:Get(i) then
+                auto_strafe:Set(false)
+                return
+            end
+        end
     end
-
-    local get_knife = get_multiCombo(0)
-    local get_nades = get_multiCombo(1)
-    local get_scout = get_multiCombo(2)
-    local get_awp = get_multiCombo(3)
-    local get_auto = get_multiCombo(4)
-    local get_pistol = get_multiCombo(5)
-    local get_heavy = get_multiCombo(6)
-    local get_global = get_multiCombo(7)
-
-    local standing = velocity(player) < 2
-
-    -- jump fix
-    if standing then
-        if is_knife and get_knife then auto_strafe:Set(false)
-        elseif is_nade and get_nades then auto_strafe:Set(false) 
-        elseif is_ssg and get_scout then auto_strafe:Set(false) 
-        elseif is_awp and get_awp then auto_strafe:Set(false) 
-        elseif is_auto and get_auto then auto_strafe:Set(false) 
-        elseif is_pistol and get_pistol then auto_strafe:Set(false) 
-        elseif is_heavy_pistol and get_heavy then auto_strafe:Set(false)
-        elseif is_gun and get_global then auto_strafe:Set(false)
-        else auto_strafe:Set(true) end
-    else
-        auto_strafe:Set(true)
-    end
+    auto_strafe:Set(true)
 end
 
 function jump_hitchance()
+    if not hc_on:GetBool() then return end
     local player = EntityList.GetLocalPlayer()
     if player == nil then return end
-
-    local standing = velocity(player) < 5
 		
-    if hc_on:GetBool() then
-        if standing and band(player:GetProp("m_fFlags"), 1) ~= 1 then
-            for i = 1, 64 do
-                RageBot.OverrideHitchance(i , hc:GetInt())
-            end
+    if velocity(player) < 10 and bit.band(player:GetProp("m_fFlags"), 1) ~= 1 then
+        for i = 1, 64 do
+            RageBot.OverrideHitchance(i , hc:GetInt())
         end
     end
 end
 
-Cheat.RegisterCallback("prediction", function ()
+Cheat.RegisterCallback("createmove", function ()
     jump_hitchance()
     jump_fix()
-end)
-
-Cheat.RegisterCallback("draw", function()
-    if hc_on:GetBool() then
-        hc:SetVisible(true)
-    else
-        hc:SetVisible(false)
-    end
 end)
